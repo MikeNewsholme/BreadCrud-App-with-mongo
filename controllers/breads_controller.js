036974619a8a3c183ac46f31f8ff2bcd60946bd1
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-07-05 21:22:28 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -28,6 +28,13 @@ breads.get('/', (req,res)=>{
 breads.get('/new', (req,res)=>{
     res.render('new')
 })
+//edit form
+breads.get('/:arrayIndex/edit',(req,res) =>{
+    res.render('edit', {
+        bread: Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex
+    })
+})
 //Show
 breads.get('/:arrayIndex', (req,res) => {
     if(Bread[req.params.arrayIndex]){
@@ -40,10 +47,28 @@ breads.get('/:arrayIndex', (req,res) => {
     res.send('404')
 }
 })
+//edit form
+breads.get('/:arrayIndex/edit',(req,res) =>{
+    res.render('edit', {
+        bread: Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex
+    })
+})
 //delete
 breads.delete('/:arrayIndex', (req,res)=>{
     Bread.splice(req.params.arrayIndex,1)
     res.status(303).redirect('/breads')
+})
+
+//update
+breads.put('/:arrayIndex', (req,res) =>{
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
